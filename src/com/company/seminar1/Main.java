f0d5a926163e529ceaa4c2a5931e9d67e44c1fc3
dsanta <santadavid18@stud.ase.ro>
2021-02-27 22:29:48 +0200

    Updated Main.java

--- src/com/company/seminar1/Main.java
+++ src/com/company/seminar1/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World - CTS");
+        Person person = new Person("Santa","David");
+        System.out.println(person.toString());
     }
 }
