f611c5daa4b4becc6a3ac1fc20f00b8a8f98efef
dsanta <santadavid18@stud.ase.ro>
2021-03-04 14:09:07 +0200

    Added CalendarUtil.java

--- src/ro/ase/csie/cts/seminar2/Main.java
+++ src/ro/ase/csie/cts/seminar2/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         CalendarUtil cu = new CalendarUtil();
         System.out.println(cu.weekDay(3));
+        System.out.println(cu.weekDay2(3));
     }
 }
