411de7ff1a1e2f3be4fe411e0aa51562fc89e4dc
dsanta <santadavid18@stud.ase.ro>
2021-03-04 15:26:09 +0200

    Final Version

--- src/ro/ase/csie/cts/seminar2/Main.java
+++ src/ro/ase/csie/cts/seminar2/Main.java
@@ -1,6 +1,7 @@
 package ro.ase.csie.cts.seminar2;
 
 import ro.ase.csie.cts.seminar2.solid.BankAccount;
+import ro.ase.csie.cts.seminar2.solid.FeeBankAccount;
 import ro.ase.csie.cts.seminar2.solid.InsufficientFundsException;
 import ro.ase.csie.cts.seminar2.solid.Person;
 
@@ -17,5 +18,15 @@ public class Main {
             System.out.println("Insufficient Funds");
         }
         System.out.println(account.getBalance());
+
+        account = new FeeBankAccount("INGB321312312",p);
+
+        account.deposit(500);
+        try {
+            account.withdraw(100);
+        } catch (InsufficientFundsException e) {
+            System.out.println("Insufficient Funds");
+        }
+        account.getBalance();
     }
 }
