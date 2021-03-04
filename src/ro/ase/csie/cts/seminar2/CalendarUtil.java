b2dab59d9ad0f2ffa3920504149512bdd4e6abc5
dsanta <santadavid18@stud.ase.ro>
2021-03-04 14:45:30 +0200

    Added weekDay4 function

--- src/ro/ase/csie/cts/seminar2/CalendarUtil.java
+++ src/ro/ase/csie/cts/seminar2/CalendarUtil.java
@@ -1,8 +1,11 @@
 package ro.ase.csie.cts.seminar2;
 
+import java.text.DateFormatSymbols;
+import java.util.Locale;
+
 public class CalendarUtil {
 
-    public String weekDay(int day) {
+    public String weekDay(int day) throws IncorrectDayException {
         if (day == 1)
             return "Sunday";
         else if (day == 2)
@@ -18,10 +21,10 @@ public class CalendarUtil {
         else if (day == 7)
             return "Saturday";
 
-        return null;
+        throw new IncorrectDayException("Only 7 days in a week");
     }
 
-    public String weekDay2(int day){
+    public String weekDay2(int day) throws IncorrectDayException {
         switch(day){
             case 1:
                 return "Sunday";
@@ -38,12 +41,25 @@ public class CalendarUtil {
             case 7:
                 return "Saturday";
             default:
-                return null;
+                throw new IncorrectDayException("Only 7 days in a week");
         }
     }
 
-    public String weekDay3(int day){
-        String[] weekDays = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
-        return weekDays[day-1];
+    public String weekDay3(int day) throws IncorrectDayException {
+        String[] weekDays = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        if(day < 1 || day > 7) {
+            throw new IncorrectDayException("Only 7 days in a week");
+        }
+        else {
+            return weekDays[day - 1];
+        }
+    }
+
+    public String weekDay4(int day) throws IncorrectDayException {
+        if(day < 1 || day > 7) {
+            throw new IncorrectDayException("Only 7 days in a week");
+        }
+        String[] days = DateFormatSymbols.getInstance(Locale.CHINESE).getWeekdays();
+        return days[day - 1];
     }
 }
---
+++ src/ro/ase/csie/cts/seminar2/IncorrectDayException.java
@@ -0,0 +1,7 @@
+package ro.ase.csie.cts.seminar2;
+
+public class IncorrectDayException extends Exception{
+    public IncorrectDayException(String s) {
+        super(s);
+    }
+}
