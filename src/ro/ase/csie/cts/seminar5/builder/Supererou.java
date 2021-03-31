d73e9cb41b312d26b3e1bf710f3cf0734135ad54
David Santa <santadavid18@stud.ase.ro>
2021-03-31 15:13:13 +0300

    Builder done

--- src/ro/ase/csie/cts/seminar5/builder/Supererou.java
+++ src/ro/ase/csie/cts/seminar5/builder/Supererou.java
@@ -3,21 +3,21 @@ package ro.ase.csie.cts.seminar5.builder;
 
 public class Supererou {
 
-    String nume;
-    int puncteViata;
-    boolean esteNegativ;
-    boolean esteRanitGrav;
+    private String nume;
+    private int puncteViata;
+    private boolean esteNegativ;
+    private boolean esteRanitGrav;
 
-    InterfataArma armaStanga;
-    InterfataArma armaDreapta;
-    InterfataSuperputere superputere;
-    InterfataSuperputere supeSuperputere;
+    private InterfataArma armaStanga;
+    private InterfataArma armaDreapta;
+    private InterfataSuperputere superputere;
+    private InterfataSuperputere supeSuperputere;
 
-    public Supererou() {
+    private Supererou() {
 
     }
 
-    public Supererou(String nume, int puncteViata, boolean esteNegativ, boolean esteRanitGrav, InterfataArma armaStanga, InterfataArma armaDreapta, InterfataSuperputere superputere, InterfataSuperputere supeSuperputere) {
+    private Supererou(String nume, int puncteViata, boolean esteNegativ, boolean esteRanitGrav, InterfataArma armaStanga, InterfataArma armaDreapta, InterfataSuperputere superputere, InterfataSuperputere supeSuperputere) {
         this.nume = nume;
         this.puncteViata = puncteViata;
         this.esteNegativ = esteNegativ;
@@ -26,5 +26,50 @@ public class Supererou {
         this.armaDreapta = armaDreapta;
         this.superputere = superputere;
         this.supeSuperputere = supeSuperputere;
+    }
+
+    public static class SupererouBuilder {
+        Supererou supererou;
+
+        public SupererouBuilder(String nume, int puncteViata) {
+            this.supererou = new Supererou();
+            this.supererou.nume = nume;
+            this.supererou.puncteViata = puncteViata;
+        }
+
+        public SupererouBuilder esteNegativ() {
+            this.supererou.esteNegativ = true;
+            return this;
+        }
+
+        public SupererouBuilder esteRanitGrav(){
+            this.supererou.esteRanitGrav = true;
+            return this;
+        }
+
+        public SupererouBuilder setArmaStanga(InterfataArma arma){
+            this.supererou.armaStanga = arma;
+            return this;
+        }
+
+        public SupererouBuilder setArmaDreapta(InterfataArma arma){
+            this.supererou.armaDreapta = arma;
+            return this;
+        }
+
+        public SupererouBuilder setSuperputere(InterfataSuperputere superputere){
+            this.supererou.superputere = superputere;
+            return this;
+        }
+
+        public SupererouBuilder setSuperSuperputere(InterfataSuperputere superputere){
+            this.supererou.supeSuperputere = superputere;
+            return this;
+        }
+
+        public Supererou build(){
+            return this.supererou;
+        }
+
     }
 }
--- src/ro/ase/csie/cts/seminar5/builder/TestBuilder.java
+++ src/ro/ase/csie/cts/seminar5/builder/TestBuilder.java
@@ -2,12 +2,22 @@ package ro.ase.csie.cts.seminar5.builder;
 
 public class TestBuilder {
     public static void main(String[] args) {
-        //1. construire obiect
-        Supererou supererou = new Supererou();
-        //2. initializare
-        supererou.nume = "Tecsor Bogdan";
+//        1. construire obiect
+//        Supererou supererou = new Supererou();
+//        2. initializare
+//        supererou.nume = "Tecsor Bogdan";
+//
+//        new Supererou("Tatu Andrei",100,false,false,new Arma(),null,null,null);
 
-        new Supererou("Tatu Andrei",100,false,false,new Arma(),null,null,null);
 
+        Supererou superTecsor = new Supererou.SupererouBuilder("superTecsor",100)
+                .setArmaDreapta(new Arma())
+                .build();
+
+        Supererou superBodi = new Supererou.SupererouBuilder("superBodi",200)
+                .esteNegativ()
+                .esteRanitGrav()
+                .setArmaDreapta(new Arma())
+                .build();
     }
 }
