11db824b45f26c0584ccd3aacc9d829edc9f4232
David Santa <santadavid18@stud.ase.ro>
2021-05-19 15:29:28 +0300

    final seminar 12

--- src/ro/ase/csie/cts/seminar12/testare/teste/OperatiiMatematiceTest.java
+++ src/ro/ase/csie/cts/seminar12/testare/teste/OperatiiMatematiceTest.java
@@ -1,5 +1,7 @@
 package ro.ase.csie.cts.seminar12.testare.teste;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import ro.ase.csie.cts.seminar12.testare.modele.OperatiiMatematice;
 
@@ -7,14 +9,27 @@ import static org.junit.Assert.*;
 
 public class OperatiiMatematiceTest {
 
+    @BeforeClass
+    public static void setUpBeforeClass(){
+        System.out.println("salut");
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass(){
+        System.out.println("La revedere");
+    }
 
     @org.junit.Before
     public void setUp() throws Exception {
+        System.out.println("Pregatire Test");
     }
 
     @org.junit.After
     public void tearDown() throws Exception {
+        System.out.println("Stergere Resurse Test");
     }
+
+
 
     @Test
     public void test() {
