0cb259bd331487e0448837c122b3395794ad0110
David Santa <santadavid18@stud.ase.ro>
2021-05-26 14:47:25 +0300

    pauza seminar 13

---
+++ src/ro/ase/csie/cts/seminar13/testare/exceptii/ExceptieNume.java
@@ -0,0 +1,4 @@
+package ro.ase.csie.cts.seminar13.testare.exceptii;
+
+public class ExceptieNume extends Exception {
+}
---
+++ src/ro/ase/csie/cts/seminar13/testare/exceptii/ExceptieVarsta.java
@@ -0,0 +1,4 @@
+package ro.ase.csie.cts.seminar13.testare.exceptii;
+
+public class ExceptieVarsta extends Exception{
+}
